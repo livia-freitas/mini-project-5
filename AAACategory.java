de11a8cbfd9ed4ff715d639d9c985e1ba143fc90
Livia S. Freitas <138071569+livia-freitas@users.noreply.github.com>
2023-10-22 16:06:35 -0500

    Added stubs and documentation

--- AAACategory.java
+++ AAACategory.java
@@ -10,4 +10,52 @@ package mp5;
 
 public class AAACategory {
   
+  AssociativeArray<String,String> location_to_text;
+  String category_name;
+
+  public AAACategory(String name){
+    this.category_name = name;
+    this.location_to_text = new AssociativeArray<>();
+  }
+
+  /**
+   * Adds the mapping of the imageLoc to the text to the category.
+   */
+  public void addItem(String imageLoc, String text){
+
+  }
+
+  /**
+   * Returns the name of the category
+   */
+  public String getCategory(){
+    //STUB
+    return new String();
+  }
+
+  /**
+   * Returns an array of all the images in the category
+   * @return
+   */
+    public String[] getImages(){
+    //STUB
+    return new String[1];
+  }
+
+  /**
+   * Returns the text associated with the given image loc in this category
+   */
+    public String getText(){
+    //STUB
+    return new String();
+  }
+
+  /**
+   * Determines if the provided image is stored in the category
+   */
+  public boolean hasImage(){
+    //STUB
+    return true;
+  }
+  
 }
